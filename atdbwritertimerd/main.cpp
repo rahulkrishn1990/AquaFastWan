a62cddf877404301ddd11a31e58783a9ef7beeac
Gangadhar Bobbili <Gangadhar.Bobbili@hotmail.com>
2018-06-10 21:33:13 +0200

    kdjfljdf

--- atdbwritertimerd/main.cpp
+++ atdbwritertimerd/main.cpp
@@ -10,7 +10,6 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	QCoreApplication a(argc, argv);
-
 	if (argc < 2)
 	{
 		qDebug() << "Usage: " << argv[0] << "<Type>:<Intervel> <Type>:<Intervel> ...";
@@ -19,29 +18,27 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	AtDbWriterTimer qtDbWriterTimer;
-	qtDbWriterTimer.fire("atphclient", 1);
-
-//	for (int i = 1; i <= argc; i++)
-//	{
-//		QString type(QString::fromStdString(argv[i]).split(":").at(0));
-//		int interval = QString::fromStdString(argv[i]).split(":").at(1).toInt();
-//		if (type.isEmpty() == false && interval > 0)
-//		{
-//			if (type == "Ph")
-//			{
-//				qDebug() << "Starting atphclient with Interval: " << interval;
-//				AtDbWriterTimer qtDbWriterTimer;
-//				qtDbWriterTimer.fire("atphclient", interval);
-//			}
-//			if (type == "Salinity")
-//			{
-//				qDebug() << "Starting atphclient with Interval: " << interval;
-//				AtDbWriterTimer qtDbWriterTimer;
-//				qtDbWriterTimer.fire("atsalinityclient", interval);
-//			}
-//		}
-//	}
+	AtDbWriterTimer* qtDbWriterTimer;
+	for (int i=1;i<argc;i++)
+	{
+		QString type = QString::fromStdString(argv[i]).split(":").at(0);
+		int interval = QString::fromStdString(argv[i]).split(":").at(1).toInt();
+		if (type.isEmpty() == false && interval > 0)
+		{
+			if (type == "Ph")
+			{
+				qDebug() << "Starting atphclient with Interval: " << interval;
+				qtDbWriterTimer = new AtDbWriterTimer();
+				qtDbWriterTimer->fire("atphclient", interval);
+			}
+			if (type == "Salinity")
+			{
+				qDebug() << "Starting atsalinityclient with Interval: " << interval;
+				qtDbWriterTimer = new AtDbWriterTimer();
+				qtDbWriterTimer->fire("atsalinityclient", interval);
+			}
+		}
+	}
 
 	return a.exec();
 }
